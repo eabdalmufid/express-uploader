946575683bd01ac433b23ce8524a7cd3ee2bf98b
eabdalmufid <eabdalmufid@gmail.com>
2024-01-20 10:33:02 +0700

    first

---
+++ README.md
@@ -0,0 +1,41 @@
+<p align="center">
+<img src="https://raw.githubusercontent.com/Xinz-Team/XinzBot/main/media/Itsuki.jpg" alt="XINZ BOT" width="128" height="128"/>
+</p>
+<p align="center">
+<a href="#"><img title="Express Uploader" src="https://img.shields.io/badge/Express Uploader-green?colorA=%23ff0000&colorB=%23017e40&style=for-the-badge"></a>
+</p>
+<p align="center">
+<a href="https://github.com/eabdalmufid"><img title="Author" src="https://img.shields.io/badge/Author-eabdalmufid-red.svg?style=for-the-badge&logo=github"></a>
+</p>
+<p align="center">
+<a href="https://github.com/eabdalmufid/followers"><img title="Followers" src="https://img.shields.io/github/followers/eabdalmufid?color=blue&style=flat-square"></a>
+<a href="https://github.com/eabdalmufid/megumikato2/stargazers/"><img title="Stars" src="https://img.shields.io/github/stars/eabdalmufid/express-uploader?color=red&style=flat-square"></a>
+<a href="https://github.com/eabdalmufid/megumikato2/network/members"><img title="Forks" src="https://img.shields.io/github/forks/eabdalmufid/express-uploader?color=red&style=flat-square"></a>
+<a href="https://github.com/eabdalmufid/megumikato2/watchers"><img title="Watching" src="https://img.shields.io/github/watchers/eabdalmufid/express-uploader?label=Watchers&color=blue&style=flat-square"></a>
+</p>
+
+---
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/eabdalmufid/express-uploader/)
+
+[![Run on Repl.it](https://repl.it/badge/github/eabdalmufid/express-uploader)](https://repl.it/github/eabdalmufid/express-uploader)
+
+# Install
+
+```bash
+> git clone https://github.com/eabdalmufid/express-uploader
+> cd express-uploader
+> npm i
+```
+
+# Start
+
+```bash
+> npm start
+```
+
+Check your site in [http://localhost:8000](http://localhost:8000)
+
+# Donation
+
+[QRIS](https://api.ordiston.xyz/qris)
---
+++ app.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const secure = require('ssl-express-www');
+const app = express()
+const port = process.env.PORT || 8000
+
+// Cretae folder
+if (!fs.existsSync('./public/file')) fs.mkdirSync('./public/file')
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
+app.set('json spaces', 2)
+app.use(cors())
+//app.use(logger('dev'))
+app.use(secure)
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(cookieParser())
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+const storage = multer.diskStorage({
+    destination: 'public/file',
+    filename: (req, file, cb) => {
+        cb(null, makeid(12) +
+            path.extname(file.originalname))
+    }
+});
+
+const upload = multer({
+    storage,
+    limits: {
+        fileSize: 10000000 // 10 MB
+    }
+})
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile('./public/index.html')
+})
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    if (!req.file.path) return res.status(400).json({
+        status: false,
+        message: "No file uploaded"
+    })
+    res.status(200).json({
+        status: true,
+        message: "Created by afidev",
+        result: {
+            originalname: req.file.originalname,
+            encoding: req.file.encoding,
+            mimetype: req.file.mimetype,
+            size: req.file.size,
+            url: "https://" + req.hostname + "/file/" + req.file.filename
+        }
+    })
+}, (error, req, res, next) => {
+    res.status(400).json({
+        error: error.message
+    })
+})
+
+app.post('/multi-upload', upload.array('files', 10), (req, res) => {
+    if (!req.files) return res.status(400).json({
+        status: false,
+        message: "No file uploaded"
+    })
+    const result = []
+    req.files.forEach(v => {
+        result.push({
+            originalname: v.originalname,
+            encoding: v.encoding,
+            mimetype: v.mimetype,
+            size: v.size,
+            url: "https://" + req.hostname + "/file/" + v.filename
+        })
+    });
+    res.status(200).json({
+        status: true,
+        message: "Created by afidev",
+        result: result
+    })
+})
+
+// Handling 404
+app.use(function (req, res, next) {
+    res.status(404).json({
+        status: false,
+        message: "Page not found"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
